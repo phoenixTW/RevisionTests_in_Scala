ffe61fd510d29b0fc2d4014619f00a3c44791abe
Kaustav Chakraborty <kaustav.ron@gmail.com>
2015-04-23 01:16:51 +0530

    LogicalConversion.readOcal() has been created. LogicalConversion.convertBinaryToDecimal has been modified with changing the name as convertToDecimal

--- app/com/mathematics/LogicalConversion.scala
+++ app/com/mathematics/LogicalConversion.scala
@@ -1,22 +1,27 @@
 package com.mathematics
 
 object LogicalConversion {
-
+  def readOctal(inOctal: Int) = {
+    val intOfInBinary = getIntRepresentation(inOctal)
+    val numberOfBits = intOfInBinary.toString.toCharArray.length
+    convertToDecimal(intOfInBinary, numberOfBits, 8)
+  }
+  
   def readBinary(inBinary: Any) = {
     val intOfInBinary = getIntRepresentation(inBinary)
     val numberOfBits = intOfInBinary.toString.toCharArray.length
-    convertDecimalToBinary(intOfInBinary, numberOfBits)
+    convertToDecimal(intOfInBinary, numberOfBits, 2)
   }
 
-  private def convertDecimalToBinary(intOfInBinary: Int, numberOfBits: Int): Int = {
+  private def convertToDecimal(intOfRep: Int, numberOfBits: Int, base: Int): Int = {
     var toDecimal = 0
-    var inBinary = intOfInBinary
+    var inRep = intOfRep
     for (counter <- 0 to (numberOfBits - 1)) {
-      val bit = inBinary % 10
-      val radix: Int = Math.pow(2, counter.toDouble).toInt
+      val bit = inRep % 10
+      val radix: Int = Math.pow(base, counter.toDouble).toInt
 
       toDecimal += bit * radix
-      inBinary /= 10
+      inRep /= 10
     }
 
     toDecimal
