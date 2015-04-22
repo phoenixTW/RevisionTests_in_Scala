3c1bc457c09a9d6de1d488030b984d1cce299429
Kaustav Chakraborty <kaustav.ron@gmail.com>
2015-04-23 01:04:04 +0530

    LogicalConversionTest class has been modified. Test are written according to Binary to Decimal Conversion for int type. LogicalConversion became a method object. That object contains readBinary(), private type of convertDecimalToBinary() and getIntReprasentaion() methods.

--- app/com/mathematics/LogicalConversion.scala
+++ app/com/mathematics/LogicalConversion.scala
@@ -1,8 +1,30 @@
 package com.mathematics
 
-class LogicalConversion {
-  def readBinary(decimalValue: Any) = {
-    val inDecimal = decimalValue.toString.toInt
-    Integer.toBinaryString(inDecimal).toInt
+object LogicalConversion {
+
+  def readBinary(inBinary: Any) = {
+    val intOfInBinary = getIntRepresentation(inBinary)
+    val numberOfBits = intOfInBinary.toString.toCharArray.length
+    convertDecimalToBinary(intOfInBinary, numberOfBits)
+  }
+
+  private def convertDecimalToBinary(intOfInBinary: Int, numberOfBits: Int): Int = {
+    var toDecimal = 0
+    var inBinary = intOfInBinary
+    for (counter <- 0 to (numberOfBits - 1)) {
+      val bit = inBinary % 10
+      val radix: Int = Math.pow(2, counter.toDouble).toInt
+
+      toDecimal += bit * radix
+      inBinary /= 10
+    }
+
+    toDecimal
+  }
+
+  private def getIntRepresentation(inBinary: Any): Int = inBinary match {
+    case inBinary: Int => inBinary
+    case inBinary: String => inBinary.toInt
+    case _ => 0
   }
 }
